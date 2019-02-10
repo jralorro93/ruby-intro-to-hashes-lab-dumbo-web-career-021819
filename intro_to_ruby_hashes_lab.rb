9d8e57c69807b67481d5471ad8f1d8bf6386f372
Juan Alorro <jralorro93@gmail.com>
2019-02-10 21:30:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {
-	  :pieces => 4, :names => {}, :rent_in_dolllars => {}
+	  :pieces => 4, 
+	  :names => {}, 
+	  :rent_in_dolllars => {}
 	  }
 	}
 end
